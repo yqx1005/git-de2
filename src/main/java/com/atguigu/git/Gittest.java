fbfe9ddbe19dc23e258a7cabafc7e16627b29d26
yqx1005 <99732782+yqx1005@users.noreply.github.com>
2022-02-15 22:37:15 +0800

    Update Gittest.java

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -8,5 +8,6 @@ public class Gittest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
