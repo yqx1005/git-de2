e1be7ff07dae009249159ecc8f76f77b34ae0f6a
yezi <yezi@atguigu.com>
2022-02-15 22:06:09 +0800

    hot fix commit2

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -1,10 +1,13 @@
 package com.atguigu.git;
 
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
     }
 }
